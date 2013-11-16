a82ed56c3317bf060e9122a768b9f2efebfd6193
Adam Faulkner <adamfaulkner1@gmail.com>
2013-11-15 20:20:11 -0600

    Added /file/fileid endpoint

--- site/backend.js
+++ site/backend.js
@@ -11,9 +11,24 @@ var conString = "postgres://postgres@localhost/hacktx";
 var client = new pg.Client(conString);
 var query = Q.nbind(client.query, client);
 
-// function get_max_fileid() {
-//   return query('SELECT MAX(file_id) as id FROM files');
-// }
+function get_peers_for_fileid(file_id) {
+  return query('SELECT peer_id FROM peers WHERE file_id=$1', [file_id]).
+    then(function(result) {
+      var retval = result.rows.map(function(row) { return row.peer_id; });
+      return retval;
+    })
+    .catch(function(err) {
+      console.log('Error getting peers:' , err);
+    });
+}
+
+function get_file_size(file_id) {
+  return query('SELECT size FROM files WHERE file_id=$1', [file_id])
+    .then(
+      function(result) {
+        return result.rows[0].size;
+      });
+}
 
 Q.ninvoke(client, "connect").then(
   function() {
@@ -35,7 +50,14 @@ Q.ninvoke(client, "connect").then(
 
     app.get('/file/:file_id', function(req, res) {
       var file_id = req.params.file_id;
-      res.send('NOT IMPLEMENTED');
+      var size_query = get_file_size(file_id);
+      var peers_query = get_peers_for_fileid(file_id);
+      Q.all([size_query, peers_query]).then(function(results) {
+        var size = results[0];
+        var peers = results[1];
+        res.send({size: size,
+                  peers: peers});
+      });
     });
 
     app.get('/subscribe/:file_id', function(req, res) {
