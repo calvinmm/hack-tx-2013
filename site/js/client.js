4e00abaf7e92c6b32ed1c17f3349665861a7f814
Adam Faulkner <adamfaulkner1@gmail.com>
2013-11-16 10:25:08 -0600

    Fix basic client bugs and surplus serialization

--- site/js/client.js
+++ site/js/client.js
@@ -43,7 +43,7 @@ var message_types = {
 };
 
 function masterStart(filesToUpload) {
-  if (filesToUpload.length == 0) {
+  if (filesToUpload.length === 0) {
     return;
   }
   registerRoom().then(function(room_id) {
@@ -98,7 +98,7 @@ function getParticipants() {
       break;
     }
   }
-  if (file_id == "") {
+  if (file_id === "") {
     return;
   }
   $.get(API_HOST + "/subscribe/" + file_id + "?peer_id=" + me, function(data) {
@@ -232,7 +232,7 @@ function masterAddedFile(file, file_id) {
   finished_files[file_id] = true;
 }
 
-var DELAY = 120
+var DELAY = 120;
 
 // Sends a message to another user
 function sendMessage(message, rec) {
@@ -466,12 +466,12 @@ function sendSlicedBlock(file_id, block_num, rec) {
   var reader = new FileReader();
   reader.onloadend = function(evt) {
     if (evt.target.readyState == FileReader.DONE) {
-      var newBlob = new Blob([evt.target.result], {type: file.type});
+      //var newBlob = new Blob([evt.target.result], {type: file.type});
       var m = {
         file_id: file_id,
         type: message_types.RES_BLOCK,
         block_num: block_num,
-        data: newBlob,
+        data: evt.target.result,
         data_type: file.type
       };
       sendMessage(m, rec);
